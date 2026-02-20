80ccde07387ee4c39d61696f0f42d137a6f675dc
AmrYasserG <mr.amryasser18@gmail.com>
2026-02-20 11:15:08 +0200

    QuestionsPage semi finalized

--- Questions.js
+++ Questions.js
@@ -1,3 +1,4 @@
+//questions-------------------------------------
 var questions = [
     {
         question: "What is the capital of Canada?",
@@ -50,11 +51,164 @@ var questions = [
         answer: "Australia"
     }
 ];
+var answers = new Array(10);
+var markedQuesArr = new Array(10);
 
 
-questions.sort(() => Math.random() - 0.5);
-for (let i = 0; i < questions.length; i++) {
-  questions[i].choices.sort(() => Math.random() - 0.5);
+//randomize questions and answers---------------
+// questions.sort(() => Math.random() - 0.5);
+// for (let i = 0; i < questions.length; i++) {
+//     questions[i].choices.sort(() => Math.random() - 0.5);
+// }
+
+//handle timer & progress bar --------------------
+var mins = document.getElementById('timer-mins');
+var secs = document.getElementById('timer-secs');
+var progressBar = document.getElementById('progress-bar');
+var totalTime = 10 * 60;        //10mins 
+var timeLeft = 10 * 60;
+function updateTime() {
+    mins.style.setProperty('--value', Math.floor(timeLeft / 60));
+    secs.style.setProperty('--value', timeLeft % 60);
+    timeLeft--;
 }
-console.log(questions);
+function handleProgressBar() {
+    progressBar.value = (timeLeft / totalTime) * 100;
+    var flag2Thirds = false;
+    var flagThird = false;
+    if (!flag2Thirds && timeLeft <= 2 * (totalTime / 3)) {
+        flag2Thirds = true;
+        progressBar.classList.add('progress-warning')
+    }
+    if (!flagThird && timeLeft <= (totalTime / 3)) {
+        flagThird = true;
+        progressBar.classList.add('progress-error');
+
+    }
+}
+
+// var interval = setInterval(() => {
+//     if (timeLeft >= 0) {
+//         handleProgressBar();
+//         updateTime();
+//     }
+//     else {
+//         clearInterval(interval)
+//     }//handle time out logic
+// }, 1000)
+
+//handle choice logic------------------------------
+var input1 = document.getElementById('input-1');
+var input2 = document.getElementById('input-2');
+var input3 = document.getElementById('input-3');
+var input4 = document.getElementById('input-4');
+function selectAnswer(input) {
+    input1.checked = false;
+    input2.checked = false;
+    input3.checked = false;
+    input4.checked = false;
+    if (input != null) {
+        answers[Number(questionNum.textContent) - 1] = [input.id.split('-')[1], input.labels[0].textContent]
+        document.getElementById(`input-${input.id.split('-')[1]}`).checked = true;
+    }
+    else {
+        if (answers[[Number(questionNum.textContent) - 1]] != null)
+            document.getElementById(`input-${answers[Number(questionNum.textContent) - 1][0]}`).checked = true;
+    }
+}
+
+//handle showing questions----------------------
+var questionNum = document.getElementById('question-num');
+var questionText = document.getElementById('question-text');
+var choice1 = document.getElementById('choice-1');
+var choice2 = document.getElementById('choice-2');
+var choice3 = document.getElementById('choice-3');
+var choice4 = document.getElementById('choice-4');
+
+function showQuestion(num) {
+    questionNum.textContent = num;
+    questionText.textContent = questions[num - 1].question;
+    choice1.textContent = questions[num - 1].choices[0];
+    choice2.textContent = questions[num - 1].choices[1];
+    choice3.textContent = questions[num - 1].choices[2];
+    choice4.textContent = questions[num - 1].choices[3];
+
+    selectAnswer(null);
+}
+showQuestion(1);
+
+
+//handle mark button-----------------------------
+var markButton = document.getElementById('mark-button');
+var markedButton = document.getElementById('marked-button');
+
+markButton.addEventListener('click', function () {
+    mark();
+    addMarkedQues();
+})
+markedButton.addEventListener('click', function () {
+    unmark();
+    removeMarkedQues();
+})
+
+function mark() {
+    markButton.classList.add('hidden');
+    markedButton.classList.remove('hidden');
+}
+function unmark() {
+    markButton.classList.remove('hidden');
+    markedButton.classList.add('hidden');
+}
+
+function addMarkedQues() {
+    var container = document.getElementById('marked-ques-container')
+    var markedQuesButton = document.createElement('button');
+    markedQuesButton.classList.add("w-12", "h-12", "border-1", "border-gray-300", "rounded-xl", "hover:bg-gray-300", "active:bg-transparent");
+    // markedQuesButton.classList.add("w-12", "h-12", "border-1", "border-gray-300", "rounded-xl");
+    markedQuesButton.id = 'Q' + Number(questionNum.textContent);
+    markedQuesButton.textContent = 'Q' + Number(questionNum.textContent);
+    markedQuesButton.addEventListener('click', function () {
+        showQuestion((this.id).split('Q')[1]);
+        mark();
+    });
+    markedQuesArr[Number(questionNum.textContent) - 1] = true;
+    container.append(markedQuesButton);
+}
+function removeMarkedQues() {
+    var markedQuesButton = document.getElementById('Q' + Number(questionNum.textContent))
+    markedQuesButton.remove();
+    markedQuesArr[Number(questionNum.textContent) - 1] = false;
+}
+
+//handle next and prev buttons--------------------
+var nextQues = document.getElementById('next-ques');
+var prevQues = document.getElementById('prev-ques');
+
+nextQues.addEventListener('click', function () {
+    input1.checked = false;
+    input2.checked = false;
+    input3.checked = false;
+    input4.checked = false;
+    var currQues = Number(questionNum.textContent);
+    if (currQues < 10)
+        showQuestion(currQues + 1)
+    if (markedQuesArr[Number(questionNum.textContent) - 1] == true)
+        mark();
+    else
+        unmark()
+})
+prevQues.addEventListener('click', function () {
+    input1.checked = false;
+    input2.checked = false;
+    input3.checked = false;
+    input4.checked = false;
+    var currQues = Number(questionNum.textContent);
+    if (currQues > 1)
+        showQuestion(currQues - 1)
+    if (markedQuesArr[Number(questionNum.textContent) - 1] == true)
+        mark();
+    else
+        unmark();
+})
+
 
