49c665668261805763364e6724f4f33795df159d
karin-Usama <karinusama182002@gmail.com>
2026-02-22 18:17:48 +0200

    update3

--- index.js
+++ index.js
@@ -11,4 +11,31 @@ signOutBtn.addEventListener('click', function () {
     sessionStorage.removeItem('user');
 
     window.location.replace('LoginPage.html');
+
+    
 });
+
+ var openModalBtn = document.getElementById('openModal');
+    var modal = document.getElementById('examModal');
+    var cancelBtn = document.getElementById('cancelModal');
+
+    openModalBtn.addEventListener('click', () => {
+      modal.classList.remove('hidden');
+    });
+
+    cancelBtn.addEventListener('click', () => {
+      modal.classList.add('hidden');
+    });
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.classList.add('hidden');
+      }
+    });
+
+        modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.classList.add('hidden');
+      }
+    });
+
